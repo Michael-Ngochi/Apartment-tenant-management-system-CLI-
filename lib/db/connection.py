474e80f7487f6eddde11e75b17dfd5d7bdc2c814
Michael-Ngochi <ngoochimike@gmail.com>
2025-05-27 10:23:02 +0300

    Added db connection

---
+++ lib/db/connection.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///articles.db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
